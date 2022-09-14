df43d6d4b8ce49aac359c55869d53e97b4742105
levente0509 <b.levente2@gmail.com>
2022-09-14 12:13:32 +0200

    function, loadevent

--- frontend/script.js
+++ frontend/script.js
@@ -5,21 +5,36 @@ függvényen kívül is látható/elérhető, így összeakadhat mindenhol -> el
 */
 
 let myVariable;
-console.log(myVariable);
+// console.log(myVariable);
 
 myVariable = "newValue"
-console.log(myVariable);
+// console.log(myVariable);
 
 myVariable = 2
-console.log(myVariable);
+// console.log(myVariable);
 
 myVariable = true
-console.log(myVariable);
+// console.log(myVariable);
 
 let myVariable2 = "szöveg"
 
 myVariable = myVariable2
-console.log(myVariable);
+// console.log(myVariable);
 
 let myVariable3 = myVariable+myVariable2
-console.log(myVariable3);
+// console.log(myVariable3);
+
+function sumVariables(variable1, variable2) {
+console.log(variable1 + variable2)
+}
+sumVariables(myVariable,myVariable2);
+sumVariables(8,6);
+sumVariables("word1","word2");
+
+function loadEvent() {
+    console.log("loaded");
+    let rootElement = document.getElementById("root");
+    rootElement.insertAdjacentHTML("beforeend", "Hello There");
+}
+
+window.addEventListener("click", loadEvent());
