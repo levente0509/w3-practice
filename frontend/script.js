5553d915fce24447f5f1dfc902d116758b233bdc
levente0509 <b.levente2@gmail.com>
2022-09-15 11:48:33 +0200

    cats

--- frontend/script.js
+++ frontend/script.js
@@ -4,37 +4,52 @@
 függvényen kívül is látható/elérhető, így összeakadhat mindenhol -> elavult technológia
 */
 
-let myVariable;
-// console.log(myVariable);
+// let myVariable;
+// // console.log(myVariable);
 
-myVariable = "newValue"
-// console.log(myVariable);
+// myVariable = "newValue"
+// // console.log(myVariable);
 
-myVariable = 2
-// console.log(myVariable);
+// myVariable = 2
+// // console.log(myVariable);
 
-myVariable = true
-// console.log(myVariable);
+// myVariable = true
+// // console.log(myVariable);
 
-let myVariable2 = "szöveg"
+// let myVariable2 = "szöveg"
 
-myVariable = myVariable2
-// console.log(myVariable);
+// myVariable = myVariable2
+// // console.log(myVariable);
 
-let myVariable3 = myVariable+myVariable2
-// console.log(myVariable3);
+// let myVariable3 = myVariable+myVariable2
+// // console.log(myVariable3);
 
-function sumVariables(variable1, variable2) {
-console.log(variable1 + variable2)
-}
-sumVariables(myVariable,myVariable2);
-sumVariables(8,6);
-sumVariables("word1","word2");
+// function sumVariables(variable1, variable2) {
+// console.log(variable1 + variable2)
+// }
+// sumVariables(myVariable,myVariable2);
+// sumVariables(8,6);
+// sumVariables("word1","word2");
+
+// function loadEvent() {
+//     console.log("loaded");
+//     let rootElement = document.getElementById("root");
+//     rootElement.insertAdjacentHTML("beforeend", "Hello There");
+// }
+
+// window.addEventListener("click", loadEvent());
+
+
+// CATS START HERE
 
 function loadEvent() {
-    console.log("loaded");
-    let rootElement = document.getElementById("root");
-    rootElement.insertAdjacentHTML("beforeend", "Hello There");
+const newCatButton = document.querySelector(".add-cat");
+newCatButton.addEventListener("click", function () {
+const catContainer = document.querySelector(".cats");
+const now = new Date ().getTime();
+const newCat = `<img src="https://cataas.com/cat?random=${now}" alt=""/>`;
+catContainer.insertAdjacentHTML("beforeend", newCat);
+})
 }
 
-window.addEventListener("click", loadEvent());
+window.addEventListener("load", loadEvent);
